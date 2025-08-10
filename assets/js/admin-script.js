ed2e07783c030820c441f82d2e4afc74c30ca797
DM Raihan <130963157+DMRaihan@users.noreply.github.com>
2025-08-10 08:08:04 +0600

    update

---
+++ assets/js/admin-script.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+    $.each($('.donate-chart'), function (indexInArray, valueOfElement) {
+        const options = {
+            series: JSON.parse($(this).attr('series')) || [50, 25, 25],
+            labels: JSON.parse($(this).attr('labels').replace(/'/g, '"')) || [
+                (1, 2, 3),
+            ],
+            legend: {
+                position: 'bottom',
+                floating: false,
+                itemMargin: {
+                    horizontal: 100,
+                    vertical: 5,
+                },
+            },
+            colors: ['#FF7675', '#6C5CE7', '#FFA62B', '#FFEAA7'],
+            title: {
+                text: this.title,
+                align: 'left',
+            },
+            chart: {
+                type: 'donut',
+            },
+            plotOptions: {
+                pie: {
+                    donut: {
+                        size: '50%',
+                        labels: {
+                            show: false,
+                            total: {
+                                show: true,
+                                label: 'Total',
+                            },
+                        },
+                    },
+                },
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            responsive: [
+                {
+                    breakpoint: 324,
+                    options: {
+                        chart: {
+                            width: 200,
+                        },
+                        legend: {
+                            position: 'bottom',
+                        },
+                    },
+                },
+            ],
+        };
+        const chart = new ApexCharts(this, options);
+        chart.render();
+    });
+
+    let table = new DataTable('#all-user-table', {
+        initComplete: function () {
+            // Access the search input field and set a placeholder
+            const searchInput = document.querySelector(
+                '[type="search"][aria-controls="all-user-table"]',
+            );
+            if (searchInput) {
+                searchInput.placeholder = 'Search for user etc...';
+                searchInput.previousSibling.innerHTML = `<i class="fa-solid fa-magnifying-glass"></i>`;
+                searchInput.previousSibling.classList.add(
+                    'all-user-table-label',
+                );
+            }
+        },
+    });
+});
